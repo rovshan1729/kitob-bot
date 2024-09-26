8d9ea8fffd8a8bb49ed9a3ad76f921e839353c99
Ravshanjon <super777imba@gmail.com>
2024-09-26 16:55:56 +0500

    fix: registration

--- tgbot/urls.py
+++ tgbot/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import home, telegram
 from src.settings import WEBHOOK_PATH
+from tgbot.views import health_check_celery, health_check_redis
+
 
 urlpatterns = [
     path('', home, name='home'),
     path(WEBHOOK_PATH, telegram, name='webhook'),
+    path("health-check/redis/", health_check_redis, name="health-check-redis"),
+    path("health-check/celery/", health_check_celery, name="health-check-celery"),
 ]
