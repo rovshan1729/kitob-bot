d7ccd88186e0266805ee3e2221f52fb62b432560
rovshan <wqe@gmail.com>
2024-11-09 23:14:01 +0500

    add: restart bot

--- tgbot/bot/handlers/users/echo.py
+++ tgbot/bot/handlers/users/echo.py
@@ -1,17 +1,20 @@
 from aiogram import types
+from aiogram.dispatcher.filters.builtin import ChatTypeFilter
+from aiogram.types import ChatType
+
 from tgbot.bot.loader import dp
-from tgbot.models import TelegramProfile
+from tgbot.bot.loader import gettext as _
 from tgbot.bot.utils import get_user
-from tgbot.bot.states.main import AdmissionState
 
 
-@dp.message_handler(commands=["restart"], state="*")
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), commands=["restart"], state="*")
 async def unregister_command(message: types.Message):
     user = get_user(message.from_user.id)
     if user.is_registered:
         user.is_registered = False
         user.save()
-        await message.answer("Your account is unregistered.")
+        await message.answer(_("Qaytadan ro‘yxatdan o‘tish uchun /start tugmasini bosing."))
     else:
-        await message.answer("You have not registered yet!")
+        await message.answer(_("Siz ro‘yxatdan o‘tib bo‘lgansiz"))
+
         
